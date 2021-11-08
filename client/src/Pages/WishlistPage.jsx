cba321f04ba6e0b24893f745308719b9eb63dceb
amandaheideman <hedieman.amanda@gmail.com>
2021-11-08 13:24:03 +0100

    add axios, connect client to server and db

--- client/src/Pages/WishlistPage.jsx
+++ client/src/Pages/WishlistPage.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const wishlistPage = () => {
-  console.log("wishlist");
+const url = axios.create({
+  baseURL: process.env.API_KEY || 'http://localhost:5000'
+});
+
+
+const WishlistPage = () => {
+
+  const [wishlist, setWishlist] = useState([]);
+
+  function getAllGifts() {
+    url.get('/wishlist')
+      .then(response => {
+        setWishlist(response.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+
+  useEffect(() => {
+    getAllGifts()
+  }, []);
+
   return (
     <div>
-      wishlist
+      Wishlist
+      {wishlist.map((value)=> {
+        return <p>{value.title}</p>
+      })}
     </div>
   )
 }
 
-export default wishlistPage
+export default WishlistPage
