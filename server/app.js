63030338957bc192b7da75d11b474cdeaac806c8
amandaheideman <hedieman.amanda@gmail.com>
2021-11-08 10:51:38 +0100

    add database

--- server/app.js
+++ server/app.js
@@ -1,14 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
-
+const app = express();
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,8 +19,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log("Connected to database"))
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
