a389a08c1167a82496539270eecccf5d6026e55b
amandaheideman <hedieman.amanda@gmail.com>
2021-11-09 09:39:12 +0100

    Add countdown on home page

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Countdown from 'react-countdown';
 
 const HomePage = () => {
+
+  let weddingDate = new Date("July 30, 2022 13:30:00").getTime();
+
   return (
     <div>
-      home
+      <h1>Johanna & Björn </h1>
+      <div className="countdown">
+        <h4>Vi gifter oss om</h4>
+        <Countdown date={weddingDate} />
+      </div>
     </div>
   )
 }
